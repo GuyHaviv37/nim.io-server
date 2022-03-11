9ef706d881635b53d3140900b6c2e6c4921e9bc7
GuyHaviv37 <guyhavivcodes@gmail.com>
2022-03-11 15:03:32 +0200

    change user/not in room error type

--- src/users/errors.js
+++ src/users/errors.js
@@ -1,5 +1,5 @@
 const UserNotFoundError = (userId) => ({type: 'NO_USER_FOUND', msg: `No user with id: ${userId} was found`});
-const UserNotInRoom = (userId) => ({type: 'USER_NOT_IN_ROOM', msg: `User with id: ${userId} is currently not in any rooms`})
+const UserNotInRoom = (userId) => ({type: 'USER_WITHOUT_ROOM', msg: `User with id: ${userId} is currently not in any rooms`})
 
 module.exports = {
     UserNotFoundError,
